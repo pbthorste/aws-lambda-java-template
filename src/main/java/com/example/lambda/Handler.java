30ced55365bc0cacc29a0ee3a41a5a3db2c9b0e5
pbthorste <pbthorste@users.noreply.github.com>
2017-02-19 11:48:02 +0100

    Change to output map (json) instead of plain string. update descriptor

--- src/main/java/com/example/lambda/Handler.java
+++ src/main/java/com/example/lambda/Handler.java
@@ -3,15 +3,17 @@ package com.example.lambda;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 
+import java.util.HashMap;
 import java.util.Map;
 
 
-public class Handler implements RequestHandler<Map<String, Object>, String> {
+public class Handler implements RequestHandler<Map<String, Object>, Map<String, String>> {
 
     @Override
-    public String handleRequest(Map<String, Object> input, Context context) {
-
-        return "hey, been trying to meet you!";
+    public Map<String, String> handleRequest(Map<String, Object> input, Context context) {
+        Map<String, String> returnable = new HashMap<>();
+        returnable.put("result", "hey, been trying to meet you!");
+        return returnable;
     }
 
 }
